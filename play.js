54a33912715e64181e498897ed5147d235c3e938
Noya Onggud <yanuo44@gmail.com>
2021-12-01 17:41:57 -0500

    setup stdin

--- play.js
+++ play.js
@@ -5,6 +5,25 @@ console.log("Connecting ...");
 connect();
 
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
 
 // Connection information:
 // IP: 165.227.47.243
